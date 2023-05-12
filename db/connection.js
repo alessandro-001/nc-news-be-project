8e6f91c98da400563dc8de0c5435925b474d16e5
alessandro-001 <alessandrofrondini@gmail.com>
2023-05-12 10:56:12 +0100

    added hosting with elephantSQL

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
+}
+
+const config = {};
+
+if(ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL
+  config.max = 2
 }
 
 module.exports = new Pool();
